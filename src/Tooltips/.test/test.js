16810c39d8e713d0c9526c0c0899c0b19d7085c7
Carol Leung <carol@LIQIDs-MBP-4.fritz.box>
2017-06-22 11:42:14 +0200

    feat: add integration test for tooltips

---
+++ src/Tooltips/.test/test.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { expect } from 'chai';
+import { shallow } from 'enzyme';
+import sinon from 'sinon';
+import Tooltips from '../index.js';
+
+const props = {
+    position: 'bottom',
+    text: 'example text',
+    children: 'hint message'
+};
+
+const container = shallow(<Tooltips {...props} />);
+
+describe('tests for <Tooltips> container', () => {
+    it('should render tooltips div', () => {
+        expect(container.length).to.eql(1);
+    });
+
+    it('should render position top class name', () => {
+        expect(container.find('.liq_tooltips__hint').hasClass('liq_tooltips__hint--bottom')).to.equal(true);
+    });
+
+    it('should contain the text passed as props', () => {
+        expect(container.find('.liq_tooltips__text').text()).to.contain('example text');
+    });
+
+    it('should contain the hint message passed as props', () => {
+        expect(container.find('.liq_tooltips__hint').text()).to.contain('hint message');
+    });
+
+    it('should stimulate on touch events', () => {
+        container.children('div').simulate('touchStart');
+        expect(container.children('div').hasClass('liq_tooltips--on-toggle')).to.equal(true);
+    });
+});
