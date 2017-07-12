321b19687d4c6dff68e3feea9d56978e7107e558
Carol Leung <carol@LIQIDs-MBP-4.fritz.box>
2017-07-12 14:49:05 +0200

    chore/fix: make tooltip visible when inside a parent div with overflow hidden

--- src/Tooltips/story.js
+++ src/Tooltips/story.js
@@ -12,10 +12,9 @@ storiesOf('Tooltips', module)
     .addWithInfo(
         'Default',
         () => (
-            <Tooltips
-                position='top'
-                text='Example'
-            >
-            {'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Temporibus animi totam, ipsam, aliquid veritatis voluptates cupiditate molestiae officia cum? Asperiores dolore tempora necessitatibus sunt nesciunt eligendi at, excepturi cumque dignissimos.'}
+        <Tooltips
+            text='Example'
+        >
+            {'Lorem ipsum dolorelit.'}
         </Tooltips>
     ));
